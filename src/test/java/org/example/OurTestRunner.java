c24994b9bd0c3e7cf142ebe4f9aea1501da44bdd
marianonavas <forceattack@protonmail.com>
2023-09-29 11:10:10 +0200

    Prepare an interesting lesson on the problems we run into in concurrent environments when we have mutable state.

---
+++ src/test/java/org/example/OurTestRunner.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class OurTestRunner {
+    public static void main(String[] args) {
+        // TODO Note for the instructor:
+        // Show here what happens when we deal with mutable state in concurrent environments
+        // Use for that the CalculatorTest
+    }
+}
