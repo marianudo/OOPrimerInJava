d9cf45f99ccf3f6be98e5cb17f7de4af3319e18b
marianonavas <forceattack@protonmail.com>
2023-09-29 10:53:48 +0200

    Homework assignment (or for class if too daunting or no time to do it): implement a Calculator class.

---
+++ src/main/java/org/example/Calculator.java
@@ -0,0 +1,46 @@
+package org.example;
+
+/*
+ ****************************
+ * TODO New assignment!!
+ *  If too daunting no worries, we can do this together in class
+ *  Implement a Calculator class.
+ *  The class Calculator will abstract over the accumulated result of basic arithmetic calculations
+ *  For that the class will hold as its state such accumulated result.
+ *  Every Calculator object will be created with an initial state = 0.
+ *  It will provide methods to:
+ *  - Addition (sum). This method will receive as a parameter a decimal value,
+ *   and change the state of the object accordingly. It must return void (no return).
+ *  - Subtraction. Same as addition
+ *  - Multiplication. Same
+ *  - Division. Same
+ *  - Square root. Same
+ *  It will also provide a method to query the accumulated result of all calculations performed
+ *  It will provide another method to reset to state of the object to zero (0)
+ */
+// New assignment for the weekend (or if too daunting, to review in class next week)
+// Implement
+public class Calculator {
+    // TODO Declare here a private attribute of type double to accumulate the result.
+    // Remember that its initial state should be zero,
+    // and the caller shouldn't be able to establish that initial state.
+    // As we want to be able to modify this state with each method call,
+    // we cannot (and shouldn't) make it final.
+
+    // TODO Create a method to add an arbitrary decimal number to the state of the Calculator
+    // Name it sum, declare a return type of 'void', and make it public
+
+    // TODO Now do the same for:
+    // TODO Subtraction (method name 'minus')
+
+    // TODO Multiplication (method name 'times')
+
+    // TODO Division (method name 'dividedBy')
+
+    // TODO Square root (method name 'sqrt')
+
+    // TODO Create a getter to query the state of the Calculator
+
+    // TODO Implement a method called reset that returns void,
+    // and sets the state of the calculator to zero (0)
+}
---
+++ src/test/java/org/example/CalculatorTest.java
@@ -0,0 +1,38 @@
+package org.example;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+// TODO Implement a test class that validates the correctness of the Calculator class logic
+// This exercise is intended to practice writing code to use objects of classes we have
+// previously implemented ourselves.
+public class CalculatorTest {
+    @Test void testSum() {
+        // TODO Create a Calculator object and test that the sum method works as expected
+    }
+
+
+    @Test void testMinus() {
+        // TODO
+    }
+
+    @Test void testTimes() {
+        // TODO
+    }
+
+    @Test void testDividedBy() {
+        // TODO
+    }
+
+    @Test void testSquareRoot() {
+        // TODO
+    }
+
+    @Test void testReset() {
+        // TODO
+    }
+
+    @Test void testCombinedOperations() {
+        // TODO
+    }
+}
